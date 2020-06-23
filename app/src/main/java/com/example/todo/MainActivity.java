cc69659218913f3df44229e4f55e299ce30b7829
ItsLaro <ivan.reor@gmail.com>
2020-06-23 18:45:46 -0400

    Created references to interact with the mutiple views on the layout

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.todo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private List<String> items;
+
+    Button addBtn;
+    EditText editItem;
+    RecyclerView recView1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        /*
+        * Behavior on startup
+        * */
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //View references
+        addBtn = findViewById(R.id.addBtn);
+        editItem = findViewById(R.id.editItem);
+        recView1 = findViewById(R.id.recView1);
+
     }
 }
