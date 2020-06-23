0b2359cbcadaafa5747582d7e782f70d3e7dbb3d
ItsLaro <ivan.reor@gmail.com>
2020-06-23 19:18:19 -0400

    Added button listener and respective method for 'Add Item'

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.todo;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -22,25 +23,43 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         /*
-        * Behavior on startup
+        * Runs on startup.
         * */
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //sets current layout to main layout
 
         //View references
         addBtn = findViewById(R.id.addBtn);
         editItem = findViewById(R.id.editItem);
         recView1 = findViewById(R.id.recView1);
 
+        //Data
         items = new ArrayList<>();
         items.add("Go to the theater");
         items.add("Visit my grandmother");
         items.add("Vacation to Punta Cana");
 
-        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        //Adapter
+        final ItemsAdapter itemsAdapter = new ItemsAdapter(items);
         recView1.setAdapter(itemsAdapter);
         recView1.setLayoutManager(new LinearLayoutManager(this));
+
+        //Listeners
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               addItem(editItem.getText().toString(), itemsAdapter);
+               editItem.setText("");
+
+            }
+        });
+    }
+
+    //CRUD Operations
+    protected void addItem(String newItem, ItemsAdapter adapter){
+        items.add(newItem);
+        adapter.notifyItemInserted(items.size()-1);
 
     }
 }
